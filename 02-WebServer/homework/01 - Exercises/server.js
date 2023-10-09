e2b1a4b2525de22447f8c6055949a2f7360080e7
PHRCOOK <pabloelleproso@gmail.com>
2023-10-09 10:11:17 -0300

    SEGUNDA HOMEWORK WEBSERVER M3

--- 02-WebServer/homework/01 - Exercises/server.js
+++ 02-WebServer/homework/01 - Exercises/server.js
@@ -1,10 +1,57 @@
-var fs = require("fs");
-var http = require("http");
-/* ⚠️ NO MODIFICAR NADA POR ENCIMA DE ESTA LÍNEA ⚠️ */
-/* AQUÍ DEBAJO PUEDES ESCRIBIR LA CONSTANTE DEL PUERTO */
-const PORT = null;
+const fs = require("fs");
+const http = require("http");
 
-/* ⚠️ LA LÍNEA SIGUIENTE TIENE QUE QUEDAR COMO ESTÁ PARA PODER EXPORTAR EL SERVIDOR ⚠️ */
-module.exports =
-  /* AQUÍ DEBAJO YA PUEDES ESCRIBIR TÚ CÓDIGO REEMPLAZANDO EL VALOR DE NULL POR EL SERVIDOR */
-  null;
+// Puerto en el que se levantará el servidor
+const PORT = 3001;
+
+// Crear el servidor HTTP
+const server = http.createServer((req, res) => {
+  console.log(`Servidor levantado en el puerto ${PORT}`);
+
+  // Ruta "/api"
+  if (req.url === "/api") {
+    // Leer el archivo "dogsData.json"
+    fs.readFile("./utils/dogsData.json", (err, data) => {
+      if (err) {
+        // Si ocurre un error al leer el archivo, devolver una respuesta con código de estado 404 y mensaje "json not found"
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("json not found");
+      } else {
+        // Si se lee el archivo correctamente, devolver el contenido como respuesta JSON
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(data);
+      }
+      return;
+    });
+  }
+  // Ruta "/allDogs"
+  else if (req.url === "/allDogs") {
+    // Leer el archivo "allDogs.html"
+    fs.readFile("./utils/allDogs.html", "utf8", (err, data) => {
+      if (err) {
+        // Si ocurre un error al leer el archivo, devolver una respuesta con código de estado 404 y mensaje "html not found"
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("html not found");
+      } else {
+        // Si se lee el archivo correctamente, devolver el contenido como respuesta HTML
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+      return;
+    });
+  }
+  // Ruta no encontrada
+  else {
+    // Si se solicita una ruta distinta a "/api" o "/allDogs", devolver una respuesta con código de estado 404 y mensaje "Route not found"
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Route not found");
+  }
+});
+
+// Escuchar en el puerto especificado y en "localhost"
+server.listen(PORT, "localhost", () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+
+// Exportar el servidor para su uso externo
+module.exports = server;
