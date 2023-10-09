9a00fca5aba1161f3863166daa9a0751bd994d16
PHRCOOK <pabloelleproso@gmail.com>
2023-10-08 23:19:05 -0300

    FIRST COMMIT

---
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -0,0 +1,29 @@
+const process = require('process');
+const commands = require('./commands/index.js');
+
+// Función para imprimir el resultado en la consola
+function print(output) {
+  const message = output.replace(/^echo /, ''); 
+  process.stdout.write(message);
+  process.stdout.write('\nprompt > ');
+}
+
+// Función principal que maneja la consola
+function bash() {
+  process.stdout.write('prompt > ');
+  process.stdin.on('data', (data) => {
+    const args = data.toString().trim();
+    const cmd = args.split(' ')[0];
+    if (!commands[cmd]) {
+      print(`command not found: ${cmd}`);
+      return;
+    }
+    commands[cmd](print, args);
+    process.stdout.write('\nprompt > ');
+  });
+}
+
+module.exports = {
+  print,
+  bash,
+};
---
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+const utils = require("../utils/request");
+const process = require("process");
+
+// Imprime el directorio de trabajo actual
+function pwd(print) {
+    print(process.cwd());
+}
+
+// Imprime la fecha y hora actual
+function date(print) {
+    print(new Date().toString());
+}
+
+// Imprime los argumentos que se le pasen
+function echo(print, args) {
+    print(args);
+}
+
+// Lista los archivos y directorios en el directorio actual
+function ls(print) {
+    fs.readdir('.', (error, files) => {
+        if (error) throw error;
+        print(files.join(' '));
+    });
+}
+
+// Muestra el contenido de un archivo
+function cat(print, args) {
+    fs.readFile(args, 'utf-8', (error, data) => {
+        if (error) throw error;
+        print(data);
+    });
+}
+
+// Muestra la primera línea de un archivo
+function head(print, args) {
+    fs.readFile(args, 'utf-8', (error, data) => {
+        if (error) throw error;
+        const lines = data.split('\n');
+        print(lines[0]);
+    });
+}
+
+// Muestra la última línea de un archivo
+function tail(print, args) {
+    fs.readFile(args, 'utf-8', (error, data) => {
+        if (error) throw error;
+        const lines = data.split('\n');
+        print(lines[lines.length - 1].trim());
+    });
+}
+
+// Realiza una solicitud HTTP a una URL y muestra la respuesta
+function curl(print, args) {
+    utils.request(args, (error, response) => {
+        if (error) throw error;
+        print(response);
+    });
+}
+
+// Exporta todas las funciones para que puedan ser utilizadas por otros módulos
+module.exports = { pwd, date, echo, ls, cat, head, tail, curl };
