19a6ed655a3e468b0cef8673dbeca25aa5ec9ae1
PHRCOOK <pabloelleproso@gmail.com>
2023-10-10 20:45:36 -0300

    CUARTA HOMEWORK ADVANCE PROMISE M3

--- 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
+++ 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
@@ -1,55 +1,128 @@
-'use strict';
+"use strict";
 /*----------------------------------------------------------------
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
 class $Promise {
   constructor(executor) {
-    if (typeof executor !== 'function') throw TypeError('Executor must be a function')
-    this._state = 'pending'
+    if (typeof executor !== "function")
+      throw TypeError("Executor must be a function");
+    this._state = "pending";
     this._value = undefined;
     this._handlerGroups = [];
-    const resolve = (value) => { this._internalResolve(value) }
-    const reject = (reason) => { this._internalReject(reason) }
-
-    executor(resolve, reject)
+    const resolve = (value) => {
+      this._internalResolve(value);
+    };
+    const reject = (reason) => {
+      this._internalReject(reason);
+    };
+    executor(resolve, reject);
   }
+
+  // Resuelve internamente la promesa
   _internalResolve(value) {
-    if (this._state !== 'pending') return;
-    this._state = "fulfilled",
-      this._value = value;
-    this._callHandlers(this._value)
-  };
+    if (this._state !== "pending") return;
+    if (value && (typeof value === "object" || typeof value === "function")) {
+      if (typeof value.then === "function") {
+        value.then(
+          this._internalResolve.bind(this),
+          this._internalReject.bind(this)
+        );
+        return;
+      }
+    }
+    this._state = "fulfilled";
+    this._value = value;
+    this._callHandlers();
+  }
+
+  // Rechaza internamente la promesa
   _internalReject(reason) {
-    if (this._state !== 'pending') return;
-    this._state = "rejected",
-      this._value = reason
-    this._callHandlers(this._value)
-  };
+    if (this._state !== "pending") return;
+    this._state = "rejected";
+    this._value = reason;
+    this._callHandlers();
+  }
 
+  // Agrega un manejador a la promesa
   then(successCb, errorCb) {
+    const downstreamPromise = new $Promise(() => {});
     const handlerGroup = {
-      successCb: typeof successCb === 'function' ? successCb : false,
-      errorCb: typeof errorCb === 'function' ? errorCb : false
+      downstreamPromise,
+      successCb: typeof successCb === "function" ? successCb : false,
+      errorCb: typeof errorCb === "function" ? errorCb : false,
+    };
+    this._handlerGroups.push(handlerGroup);
+    if (this._state !== "pending") {
+      this._callHandlers();
     }
-    this._handlerGroups.push(handlerGroup)
-    this._state !== "pending" && this._callHandlers(this._value)
+    return downstreamPromise;
   }
-  _callHandlers(value) {
+
+  // Llama a los manejadores de la promesa
+  _callHandlers() {
     while (this._handlerGroups.length) {
-      const currentHandler = this._handlerGroups.shift()
-      this._state === "fulfilled" && currentHandler.successCb && currentHandler.successCb(value)
-
-      this._state === "rejected" && currentHandler.errorCb && currentHandler.errorCb(value)
-
+      const currentHandler = this._handlerGroups.shift();
+      if (this._state === "fulfilled") {
+        if (currentHandler.successCb) {
+          try {
+            const x = currentHandler.successCb(this._value);
+            currentHandler.downstreamPromise._internalResolve(x);
+          } catch (e) {
+            currentHandler.downstreamPromise._internalReject(e);
+          }
+        } else {
+          currentHandler.downstreamPromise._internalResolve(this._value);
+        }
+      } else if (this._state === "rejected") {
+        if (currentHandler.errorCb) {
+          try {
+            const x = currentHandler.errorCb(this._value);
+            currentHandler.downstreamPromise._internalResolve(x);
+          } catch (e) {
+            currentHandler.downstreamPromise._internalReject(e);
+          }
+        } else {
+          currentHandler.downstreamPromise._internalReject(this._value);
+        }
+      }
     }
   }
+
+  // Maneja el rechazo de la promesa
   catch(errorHandler) {
-    this.then(null, errorHandler)
+    return this.then(null, errorHandler);
+  }
+
+  // Resuelve una promesa con el valor dado
+  static resolve(value) {
+    if (value instanceof this) return value;
+    return new $Promise((resolve) => resolve(value));
+  }
+
+  // Resuelve todas las promesas en paralelo
+  static all(promises) {
+    if (!Array.isArray(promises)) {
+      throw new TypeError("The argument must be an array");
+    }
+    return new $Promise((resolve, reject) => {
+      let count = promises.length;
+      const values = new Array(count);
+      if (count === 0) resolve(values);
+      promises.forEach((promise, i) => {
+        this.resolve(promise).then(
+          (value) => {
+            values[i] = value;
+            if (--count === 0) resolve(values);
+          },
+          (reason) => reject(reason)
+        );
+      });
+    });
   }
 }
-module.exports = $Promise;
 
+module.exports = $Promise;
 
 /*-------------------------------------------------------
 El spec fue diseñado para funcionar con Test'Em, por lo tanto no necesitamos
