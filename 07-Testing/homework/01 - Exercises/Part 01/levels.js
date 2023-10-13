9c1bfc10acd9c1e3226952fe5cd8ef93858dd734
PHRCOOK <pabloelleproso@gmail.com>
2023-10-13 16:31:03 -0300

    SEPTIMA HOMEWORK TESTING M3

--- 07-Testing/homework/01 - Exercises/Part 01/levels.js
+++ 07-Testing/homework/01 - Exercises/Part 01/levels.js
@@ -1,5 +1,8 @@
+// La función levelOne toma dos argumentos y devuelve su suma.
 const levelOne = (a, b) => {return a + b;};
 
+// La función levelTwo toma una cadena de texto, la divide en un array de caracteres,
+// filtra los caracteres en índices impares, y luego une los caracteres restantes de nuevo en una cadena.
 const levelTwo = (letras) => {
   return letras
       .split('')
@@ -9,10 +12,13 @@ const levelTwo = (letras) => {
       .join('');
 };
 
+// La función levelThree toma dos arrays, los concatena y luego ordena el array resultante.
 const levelThree = (a, b) => {return a.concat(b).sort();};
 
+// La función levelFour verifica si un número está incluido en el array [1729, 1, 81, 1458].
 const levelFour = (num) => {
   const henryNumbers = [1729, 1, 81, 1458];
-  return henryNumbers.includes(num);};
+  return henryNumbers.includes(num);
+};
 
 module.exports = { levelOne, levelTwo, levelThree, levelFour };
--- 07-Testing/homework/01 - Exercises/Part 02/antiTrollsSecurity.js
+++ 07-Testing/homework/01 - Exercises/Part 02/antiTrollsSecurity.js
@@ -1,14 +1,18 @@
+// La función antiTrollsSecurity elimina todas las vocales de una cadena dada.
 const antiTrollsSecurity = (string) => {
-  ;
+  // Define un array de vocales.
   const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
   let result = '';
   
+  // Itera sobre cada carácter en la cadena.
   for (let i = 0; i < string.length; i++) {
+    // Si el carácter no es una vocal, añádelo al resultado.
     if (!vowels.includes(string[i])) {
       result += string[i];
     }
   }
   
+  // Devuelve la cadena resultante sin vocales.
   return result;
 }
 
