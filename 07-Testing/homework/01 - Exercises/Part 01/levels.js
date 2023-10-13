c2397dd9f50d278beefe09b4ebc90498812335ef
PHRCOOK <pabloelleproso@gmail.com>
2023-10-13 16:22:16 -0300

    SEPTIMA HOMEWORK TEST M3

--- 07-Testing/homework/01 - Exercises/Part 01/levels.js
+++ 07-Testing/homework/01 - Exercises/Part 01/levels.js
@@ -1,9 +1,18 @@
-const levelOne = (a, b) => {};
+const levelOne = (a, b) => {return a + b;};
 
-const levelTwo = (letras) => {};
+const levelTwo = (letras) => {
+  return letras
+      .split('')
+      .filter((l, i) => {
+         return i % 2 === 0;
+      })
+      .join('');
+};
 
-const levelThree = (a, b) => {};
+const levelThree = (a, b) => {return a.concat(b).sort();};
 
-const levelFour = (num) => {};
+const levelFour = (num) => {
+  const henryNumbers = [1729, 1, 81, 1458];
+  return henryNumbers.includes(num);};
 
 module.exports = { levelOne, levelTwo, levelThree, levelFour };
--- 07-Testing/homework/01 - Exercises/Part 02/antiTrollsSecurity.js
+++ 07-Testing/homework/01 - Exercises/Part 02/antiTrollsSecurity.js
@@ -1,3 +1,15 @@
-const antiTrollsSecurity = (string) => {};
+const antiTrollsSecurity = (string) => {
+  ;
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  let result = '';
+  
+  for (let i = 0; i < string.length; i++) {
+    if (!vowels.includes(string[i])) {
+      result += string[i];
+    }
+  }
+  
+  return result;
+}
 
 module.exports = antiTrollsSecurity;
